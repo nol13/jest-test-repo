5c9ea56aa34756c67e78a48d8677c385539c0e23
Nolan Kaplan <nolan.kaplan@constructconnect.com>
2019-08-29 23:13:19 -0400

    delete more irrelevant stuff

--- babel.config.js
+++ babel.config.js
@@ -7,18 +7,7 @@ module.exports = {
           "corejs": { "version": 2 },
         }],
         "@babel/preset-react"],
-      "plugins": [
-        ["import-directory"],
-        ["@babel/plugin-proposal-decorators", { "legacy": true }],
-        ["@babel/plugin-proposal-class-properties"],
-        ["css-modules-transform", {
-          preprocessCss: "./src/test/preprocess-css-for-jest.js",
-          devMode: true,
-          generateScopedName: "[name]--[local]--[hash:base64:5]",
-          camelCase: true,
-          extensions: [".scss"]
-        }]
-      ]
+      "plugins": []
     }
   }
 };
--- scripts/postinstall.js
+++
@@ -1,8 +0,0 @@
-var shell = require('shelljs');
-var os = require('os');
-
-// fix for css module hash on windows
-if (os.platform() !== 'win32') shell.exit();
-
-shell.rm('-f', 'node_modules/babel-plugin-css-modules-transform/node_modules/generic-names/index.js');
-shell.cp('-f', 'node_modules/generic-names/index.js', 'node_modules/babel-plugin-css-modules-transform/node_modules/generic-names/index.js');
