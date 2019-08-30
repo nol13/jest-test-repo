c43259729229cd609114f2d404332a3468fd47a0
Nolan Kaplan <nolan.kaplan@constructconnect.com>
2019-08-29 22:52:26 -0400

    comment

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,8 +1,7 @@
 var shell = require('shelljs');
 var os = require('os');
 
-// node_modules/generic-names/index.js doesn't exist on build server,
-// but only need to run this on windows anyway :/
+// fix for css module hash on windows
 if (os.platform() !== 'win32') shell.exit();
 
 shell.rm('-f', 'node_modules/babel-plugin-css-modules-transform/node_modules/generic-names/index.js');
