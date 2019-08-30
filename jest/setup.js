4cce80f9e817d28a44b8b5f737afb1ea56c0fb00
Nolan Kaplan <nolan.kaplan@constructconnect.com>
2019-08-29 23:02:19 -0400

    commented code

--- jest/setup.js
+++ jest/setup.js
@@ -2,12 +2,5 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import '@babel/polyfill';
 import "regenerator-runtime/runtime";
-//import chaiAsPromised from 'chai-as-promised';
-//import chai from 'chai';
-
-
-//chai.use(chaiAsPromised);
-
-//require('chai').should();
 
 configure({ adapter: new Adapter() });
